9262f267bad307cffcd71af3384c2b7b715295f0
rajdas88 <rajdas88@gmail.com>
2013-06-22 11:12:01 +0200

    Update CourseraScraper.py

--- CourseraScraper.py
+++ CourseraScraper.py
@@ -32,6 +32,7 @@ if __name__ == '__main__':
     # http://kb.mozillazine.org/Firefox_:_FAQs_:_About:config_Entries
     profile = webdriver.FirefoxProfile(sys.argv[2])
 
+    # Setting profile parameters
     profile.set_preference("browser.download.manager.showWhenStarting",False)
     profile.set_preference("browser.download.dir", sys.argv[3])
     profile.set_preference("browser.download.folderList",2)
@@ -40,19 +41,26 @@ if __name__ == '__main__':
     profile.set_preference("plugin.disable_full_page_plugin_for_types", "application/pdf")
     profile.set_preference('browser.helperApps.alwaysAsk.force',False)
 
+    # Start Firefox webdriver
     driver = webdriver.Firefox(firefox_profile=profile)
     
+    # Go to class page
     className = sys.argv[1]
     driver.get("https://class.coursera.org/" + className + "/lecture/index")
     
+    # Wait 10 seconds for page to load
     time.sleep(10)
+    
+    # Parsing html of page
     soup = BeautifulSoup(driver.page_source)
     
     downloadLinks = []
     for link in soup.find_all('a'):
         if (link.get('href')) <> None:
+            # mp4 Files
             if (link.get('href').startswith('https://class.coursera.org/' + className + '/lecture/download.mp4')):
                 downloadLinks.append(link.get('href'))
+            # pdf files
             if(link.get('href')[-3:]) == 'pdf':
                 downloadLinks.append(link.get('href'))
   
@@ -61,7 +69,8 @@ if __name__ == '__main__':
         try:
             driver.get(item)
         except:
-            print ('Error')
+            print ('Error - check link')
             continue
 
+    # Browser must be closed manually
    
