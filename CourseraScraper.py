2742b8992a19dcc7461ad4aa85ece595cc34af81
rajdas88 <rajdas88@gmail.com>
2013-06-24 15:04:09 +0200

    Update CourseraScraper.py

--- CourseraScraper.py
+++ CourseraScraper.py
@@ -8,26 +8,6 @@ import time
 import sys
 
 if __name__ == '__main__':
-    
-    '''
-    Dependencies:
-        BeatifulSoup
-        Selenium
-    
-    Assume profile already contains log-in info & auto-download for pdf & mp4
-    In firefox, Tools -> Options -> Applications set to save file automatically
-    Args:
-    1. Class name
-        When you enter class page, it will be after "coursera.org"
-        E.g. For "https://class.coursera.org/datasci-001/class/index", class name is "datasci-001"
-    2. Profile location
-        In Windows, click Start, type "%APPDATA%\Mozilla\Firefox\Profiles" and profile is in folder with "default" in name
-        E.g. "C:\\Users\\rajd\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\r8gk2ms5.default"
-        Notice, the double slashes
-    3. Download location
-        Any folder, but again make sure to have double slashes and proper formatting
-        E.g. "C:\\Users\\rajd\\Downloads\\Lectures"
-    '''
    
     # http://kb.mozillazine.org/Firefox_:_FAQs_:_About:config_Entries
     profile = webdriver.FirefoxProfile(sys.argv[2])
